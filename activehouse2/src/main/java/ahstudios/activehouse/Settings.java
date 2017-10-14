7ac17d7812f4258d784feb28721a01dccacd1b4c
githubofryry <31857637+githubofryry@users.noreply.github.com>
2017-10-14 15:45:53 -0400

    Extends to AppCompat

--- activehouse2/src/main/java/ahstudios/activehouse/Settings.java
+++ activehouse2/src/main/java/ahstudios/activehouse/Settings.java
@@ -1,8 +1,12 @@
 package ahstudios.activehouse;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Oliver on 2017-10-04.
+ * Edited by Ryan Antolin on 2017-10-14
+ *
  */
 
-public class Settings {
+public class Settings extends AppCompatActivity {
 }
