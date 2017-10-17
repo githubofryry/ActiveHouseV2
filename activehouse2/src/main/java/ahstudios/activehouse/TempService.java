722875cb0d8fbdc60fcefecd4badf5b16aa79579
Patrick <31857589+patng2007@users.noreply.github.com>
2017-10-16 21:02:51 -0400

    Add files via upload

--- activehouse2/src/main/java/ahstudios/activehouse/TempService.java
+++ activehouse2/src/main/java/ahstudios/activehouse/TempService.java
@@ -3,30 +3,32 @@ package ahstudios.activehouse;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.os.AsyncTask;
 import android.os.IBinder;
-import android.util.Log;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class TempService extends Service {
-    public TempService() {
-    }
+public class TempService extends Service
+{
+    public TempService() {}
 
-    private static Timer timer = new Timer();
-    private Context ctx;
-    int HouseID;
 
-    public IBinder onBind(Intent arg0)
+        private static Timer timer = new Timer();
+        private Context ctx;
+        int HouseID;
+		
+	public IBinder onBind(Intent arg0)
     {
         return null;
     }
-
+        
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         HouseID = intent.getIntExtra("HouseID", 0);
         return START_STICKY;
     }
+
 
     public void onCreate()
     {
@@ -37,37 +39,35 @@ public class TempService extends Service {
 
     private void startService()
     {
-        timer.scheduleAtFixedRate(new mainTask(), 0, 60000);
+        timer.scheduleAtFixedRate(new ahstudios.activehouse.TempService.mainTask(), 0, 60000);
     }
 
     private class mainTask extends TimerTask
     {
         public void run()
         {
-            Log.e(this.getClass().getSimpleName(), "service is called");
-			new ahstudios.activehouse.TempService.getTemp().execute();   
-            
+            Log.e(this.getClass().getSimpleName(), "Service is called");
+            new ahstudios.activehouse.TempService.getTemp().execute();         
         }
     }
-
+    
     public void onDestroy()
     {
         super.onDestroy();
-        
     }
-
+    
     private class GetTemp extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            
         }
-
-        @Override
-        protected void onPostExecute(Void result) {
+	}
+	
+	
+    @Override
+    protected void onPostExecute(Void result) {
             super.onPostExecute(result);
+	}
 
-        }
-    }
 
 }
